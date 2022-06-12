043f17d1be90ae9dc34580ded1c45a7f70fc1ba9
BenjaminBruton <bbruton@wgu.edu>
2022-06-12 01:46:11 -0500

    added yaml for gcloud and message for base url

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-//const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Please append '/pokemon' to see all 150 Pokemon or '/pokemon/{idhere}' to see a particular pokemon!");
+})
 
 app.use('/pokemon', require('./routes/pokeRoutes'))
 app.use('/pokemon/:id', require('./routes/pokeRoutes'))
