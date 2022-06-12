1d4f01f09da12e952df86ba43749391ad92da0ed
BenjaminBruton <bbruton@wgu.edu>
2022-06-12 00:01:36 -0500

    JSON data added

--- backend/controllers/pokeController.js
+++ backend/controllers/pokeController.js
@@ -1,15 +1,26 @@
 const asyncHandler = require('express-async-handler')
+const jsonData = require('../data/data.json')
 
 // GET all Pokemon
 // GET /pokemon
 const getPokemon = asyncHandler(async (req, res) => {
-    res.status(200).json({ id: 1, name: "abra" })
+    res.status(200).json(jsonData);
 })
-
+//{ id: 1, name: "abra" }
 const getOnePokemon = asyncHandler(async(req, res) => {
-    res.status(200).json({ id: 2, name: "aerodactyl" })
-})
+    //const myObj = JSON.parse(jsonData);
+    var id = Number(req.params.id);
+    var pokemon = null;
 
+    jsonData.results.map(function(data) {
+        if (data.id === id) {
+        pokemon = data;
+        };
+  });
+
+  res.send({pokemon});
+});
+//pokemonList.result.req.id 
 module.exports = {
     getPokemon,
     getOnePokemon
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.use('/pokemon', require('./routes/pokeRoutes'))
-app.use('/pokemon/{id}', require('./routes/pokeRoutes'))
+app.use('/pokemon/:id', require('./routes/pokeRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
